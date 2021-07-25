a48a36f4dddd8fe1fa0effd9ba271269a61b929f
Yogesh Kumar Singh <s.ashucool14@gmail.com>
2021-07-25 19:04:00 +0530

    Create Pyramid.kt

---
+++ Pyramid.kt
@@ -0,0 +1,26 @@
+fun main() {
+    val n = 5
+    val columns = 2 * n - 1
+    val midPoint = Math.floor(columns.toDouble() / 2)
+    
+    for(row in 0..n-1){
+        var level = ""
+        
+        for(col in 0..columns-1){
+            if(midPoint-row <= col && midPoint+row >= col){
+                level += "#"
+            }else{
+                level += " "
+            }
+        }
+        println(level)
+    }
+}
+
+///////////// OUTPUT //////////////////
+
+    #    
+   ###   
+  #####  
+ ####### 
+#########
