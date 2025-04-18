1df5039ff197a42121b6027afdaf36454b4600f3
Yogesh Singh <s.ashucool14@gmail.com>
2025-04-18 19:28:57 +0530

    Update and rename rotate_array.kts to rotate_array.kt

--- rotate_array/rotate_array.kt
+++ rotate_array/rotate_array.kt
@@ -1,43 +1,34 @@
-// put n as a +ve number for left ratation
-// or
-// put n as a -ve number for right ratation
 fun rotateArray(array: ArrayList<Int>, n: Int): ArrayList<Int> {
-
     val length = array.size
+    if (length == 0) return array
 
     var k = n % length
 
+    // Handle negative rotation (right shift)
     if (k < 0) {
-        k = k + length
+        k += length
     }
 
-    var s1 = "asdasdfad"
-    for (i in s1.toCharArray())
-
-    var map = mutableMapOf<String, Int>()
-        map.containsKey("a")
+    // Step 1: Reverse first part
     reverseArray(array, 0, k - 1)
 
+    // Step 2: Reverse second part
     reverseArray(array, k, length - 1)
 
+    // Step 3: Reverse entire array
     reverseArray(array, 0, length - 1)
 
-    return array;
-
+    return array
 }
 
 fun reverseArray(array: ArrayList<Int>, start: Int, end: Int) {
-
     var s = start
     var e = end
-    var temp: Int
     while (s < e) {
-        temp = array[s]
+        val temp = array[s]
         array[s] = array[e]
         array[e] = temp
-
-        s += 1
-        e -= 1
+        s++
+        e--
     }
-
 }
